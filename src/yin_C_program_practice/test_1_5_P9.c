a8c5a23c409a505ccda56756cb23a5ee1eb207b3
useryinyinyin <2621715498@qq.com>
2022-07-11 22:57:15 +0800

    feat:C程序设计 P9 Test 1-5 DIR:Issue1

---
+++ src/yin_C_program_practice/test_1_5_P9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int fahr;
+
+    printf("this is a blank for fahr to celsius: \n");
+    for (fahr = 300; fahr >= 0; fahr -= 20) {
+        printf("fahr:%3d, celsius:%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+
+    return 0;
+}
