33eef31b10bcef5db746731bb134879686f6dadd
useryinyinyin <2621715498@qq.com>
2022-07-11 23:09:34 +0800

    feat:C程序设计 P13 Test 1-8 DIR:Issue1

---
+++ src/yin_C_program_practice/test_1_8_P13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, blanks = 0, tabs = 0, newlines = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            ++blanks;
+        } else if (c == '\t') {
+            ++tabs;
+        } else if (c == '\n') {
+            ++newlines;
+        }
+    }
+
+    printf("blanks = %d, tabs = %d, newlines = %d\n", blanks, tabs, newlines);
+
+    return 0;
+}
