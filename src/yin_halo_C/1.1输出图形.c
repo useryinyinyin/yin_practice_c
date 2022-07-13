1b21223a186d197d2d6c7cb0f9b112a7a34bd5d2
useryinyinyin <2621715498@qq.com>
2022-07-13 23:56:24 +0800

    feat:halo_C 输出图形 17 DIR:Issue1

---
+++ src/yin_halo_C/1.1输出图形.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    // 输出三角形
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+
+    printf("\n");
+    // 输出菱形
+    printf("  *\n");
+    printf(" * *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    printf("\n");
+    // 输出勾形
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+
+}
